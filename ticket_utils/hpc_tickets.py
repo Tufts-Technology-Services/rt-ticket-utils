ac2f7ba2f33e84b67bd84186e6360b7e77e77603
Chris Barnett <Christopher.Barnett@tufts.edu>
2025-08-13 14:19:59 -0400

    refactor to unify argument order and naming

---
+++ ticket_utils/hpc_tickets.py
@@ -0,0 +1,239 @@
+from ticket_utils.utils import create_ticket_header, generate_text
+
+
+def hpc_account_creation_ticket_text(project_path, user_path, 
+                                     contact, affected_client, watcher, 
+                                     assigned_to, parent_ticket_id=None):
+    """
+    Create ticket text for HPC account creation.
+
+    :param project_path: The path of the project.
+    :param user_path: The path of the user.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned.
+    :param parent_ticket_id: The ID of the parent ticket, if any.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f'HPC Account Creation | {user_path}',
+        close_notes='Cluster account created!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    notifications = generate_text('notifications')
+    t = generate_text('hpc_account_creation', user_path=user_path, 
+                      project_path=project_path, 
+                      pi_utln=affected_client, 
+                      notifications=notifications, footer=footer)
+    return ticket_header + t
+
+
+def hpc_added_to_group_ticket_text(project_path, user_path, group_name,
+                                   contact, affected_client, watcher,
+                                   assigned_to, parent_ticket_id=None):
+    """
+    Create ticket text for adding a user to a project group and adding private directory.
+
+    :param project_path: The path of the project.
+    :param home_path: The path of the user's home directory.
+    :param group_name: The name of the group.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f"Tier 1 HPC Storage: Grant Access to {user_path}",
+        close_notes='Added user to group!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    notifications = generate_text('notifications')
+    t = generate_text('hpc_added_to_group_notification', path=project_path, 
+                      user_path=user_path, group=group_name, 
+                      notifications=notifications, footer=footer)
+    return ticket_header + t
+
+
+def hpcvast_project_ticket_text(project_path: str, quota_gb: str, contact=None, 
+                                affected_client=None, watcher=None, assigned_to=None, parent_ticket_id=None):
+    """
+    Create ticket text for creating a new HPC Vast project.
+
+    :param project_path: The path of the project.
+    :param quota_gb: The quota in GB for the project.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned
+    :param parent_ticket_id: The ID of the parent ticket, if any.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f'Tier 1 HPC Storage: Creation | {quota_gb} GB | {project_path}',
+        close_notes='Project storage created!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    notifications = generate_text('notifications')
+    t = generate_text('hpc_storage', path=project_path, quota=quota_gb, 
+                      notifications=notifications, footer=footer)
+    return ticket_header + t
+
+
+def hpcvast_increase_ticket_text(project_path, quota_gb, 
+                                 contact=None, affected_client=None, watcher=None, 
+                                 assigned_to=None, parent_ticket_id=None):
+    """ Create ticket text for increasing the quota of an existing HPC Vast project.
+
+    :param project_path: The path of the project.
+    :param quota_gb: The new quota in GB for the project.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned.
+    :param parent_ticket_id: The ID of the parent ticket, if any.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f'Tier 1 HPC Storage: Increase | {quota_gb} GB | {project_path}',
+        close_notes='Quota increased!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    notifications = generate_text('notifications')
+    t = generate_text('hpc_storage_update', path=project_path, quota=quota_gb, 
+                      notifications=notifications, footer=footer)
+    return ticket_header + t
+
+
+def rstore_share_ticket_text(share_name, quota_gb, group=None, approvers=(), 
+                             contact=None, affected_client=None, watcher=None,
+                             assigned_to=None, parent_ticket_id=None):
+
+    """
+    Create ticket text for creating a new RStore share.
+
+    :param share_name: The name of the share.
+    :param quota_gb: The quota in GB for the share.
+    :param group: The group to which the share belongs.
+    :param approvers: List of approvers for the share.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned
+    :param parent_ticket_id: The ID of the parent ticket, if any.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f'Tier 1 RStore Storage: Creation | {quota_gb} GB | {share_name}',
+        close_notes='Share created!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    notifications = generate_text('notifications')
+    instructions = generate_text('map_smb_drive', share_name=share_name)
+    t = generate_text('rstore_share', share_name=share_name, quota=quota_gb, owner=affected_client, 
+                      group=group, approvers=', '.join(approvers), 
+                      notifications=notifications, footer=footer, instructions=instructions)
+    return ticket_header + t
+
+
+def rstore_increase_ticket_text(share_name, quota_gb, 
+                                contact=None, affected_client=None, watcher=None, 
+                                assigned_to=None, parent_ticket_id=None):
+    """
+    Create ticket text for increasing the quota of an existing RStore share.
+    :param share_name: The name of the share.
+    :param quota_gb: The new quota in GB for the share.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned
+    :param parent_ticket_id: The ID of the parent ticket, if any.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f'Tier 1 RStore Storage: Increase | {quota_gb} GB | {share_name}',
+        close_notes='Quota increased!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    notifications = generate_text('notifications')
+    t = generate_text('rstore_storage_update', path=share_name, quota=quota_gb, 
+                      notifications=notifications, footer=footer)
+    return ticket_header + t
+
+
+def course_directory_ticket_text(course_path: str, quota_gb: str, course_group: str, 
+                                 admin_group: str, contact=None, affected_client=None, watcher=None, 
+                                 assigned_to=None, parent_ticket_id=None):
+    """
+    Create ticket text for creating a new course directory.
+
+    :param course_path: The path of the course.
+    :param quota_gb: The quota in GB for the course.
+    :param course_group: The group name for the course.
+    :param admin_group: The admin group name for the course.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned.
+    :param parent_ticket_id: The ID of the parent ticket, if any.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f'HPC Class Storage: Creation | {quota_gb} GB | {course_path}',
+        close_notes='Course storage created!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    t = generate_text('hpc_course_storage', course_dir=course_path, quota=quota_gb, 
+                      course_group=course_group, admin_group=admin_group, footer=footer)
+    return ticket_header + t
+
+
+def course_add_ta_ticket_text(course_path: str, contact=None, affected_client=None, watcher=None, 
+                                 assigned_to=None, parent_ticket_id=None):
+    """
+    Create ticket text for creating a new course directory.
+
+    :param course_path: The path of the course.
+    :param quota_gb: The quota in GB for the course.
+    :param course_group: The group name for the course.
+    :param admin_group: The admin group name for the course.
+    :param contact: The UTLN of the contact person (requestor).
+    :param affected_client: The UTLN of the affected client.
+    :param watcher: A list of UTLNs to be added to the watch list.
+    :param assigned_to: The UTLN of the person to whom the ticket is assigned   
+    :param parent_ticket_id: The ID of the parent ticket, if any.
+    :return: A string containing the ticket text.
+    """
+    ticket_header = create_ticket_header(
+        short_description=f'HPC Class Storage: Add TA | {course_path}',
+        close_notes='Course TA added!',
+        affected_client=affected_client, contact=contact, 
+        assigned_to=assigned_to, watcher=watcher,
+        parent_ticket_id=parent_ticket_id
+    )
+    footer = generate_text('footer')
+    t = generate_text('hpc_course_add_ta', course_dir=course_path, course_name=course_path.split('/')[-1], 
+                      ta_username=affected_client, footer=footer)
+    return ticket_header + t
--- ticket_utils/models.py
+++ ticket_utils/models.py
@@ -1,23 +1,48 @@
 from pydantic import BaseModel, Field, ConfigDict
-from typing import Optional, List
+from typing import Optional
 
 
 class Ticket(BaseModel):    
     model_config = ConfigDict(extra='forbid', str_strip_whitespace=True)
     u_incident_type: str = 'Request'
-    u_contact_person: str
-    caller_id: str
+    u_contact_person: str  # requestor's utln
+    caller_id: str  # requestor's utln; affected client
     source: str = 'Email'
     category: str = 'Research'
     subcategory: str = 'Research Systems'
     u_business_service: str = 'High Performance Computing (HPC)'
     short_description: str = Field(..., alias='Short_Description')
-    assigned_to: str
+    assigned_to: str  # script user's utln
     assignment_group: str = 'TTS Research Technology'
-    parent: Optional[str] = None
-    watch_list: Optional[List[str]] = None
+    parent: Optional[str] = None  # parent ticket id
+    watch_list: Optional[list[str]] = None  # PI utln
     u_action: str = 'Activate/Setup/Install'
     state: str = 'Resolved'
     close_code: str = 'Resolved Remotely'
     close_notes: str
     message: str
+
+
+"""
+other attributes that can be used in the ticket:
+ Location	location
+ Room	u_alt_location
+ Preferred Phone Number	u_call_back_number
+ Major Incident 	u_major_incident 
+ Priority	priority
+ Due Date	due_date
+ First Call Resolution	u_first_call_resolution
+ Description	description
+ Source	contact_type
+ Configuration Item	cmdb_ci
+ Sensitive Information	u_pi_checkbox
+ Is Duplicate Incident	u_is_duplicate_incident
+ Action	u_action
+ Resolution Steps	close_notes
+ KCS Soution	u_kcs_solution
+ Resolution Code	close_code
+ Additional Comments	comments
+ Work notes	work_notes
+ Work notes watch list	u_itil_watch_list
+ Do Not Send Client Email	u_do_not_send_client_email
+"""
